5f5e6b8570a4b51825f198a125b5148dfe8999c6
Bekzod Rakhmatov <rakhmatovbekzod1996@gmail.com>
2022-01-27 16:36:01 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Aiden Choi on 2022/01/27.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
